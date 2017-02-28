ea7d58a88e5778369caa0b7881f1200bbd883f3c
HaxXorHardy <haxxorhardy@gmail.com>
2017-02-28 23:43:33 +0100

    Projektdateien hinzufügen.

---
+++ SyncFiles/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(1030, 487)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1055, 512)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ SyncFiles/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dir1 As String = "C:\Users\*\Dropbox\DropsyncFiles"
+        Dim dir2 As String = "C:\Users\*\Documents\my games\Fallout Shelter"
+        Dim sync As New myDirMonitor(dir1, dir2)
+
+    End Sub
+
+End Class
+
+
+Public Class myDirMonitor
+
+    Dim Dir1 As DirectoryInfo
+    Dim Dir2 As DirectoryInfo
+
+    Public Enum SyncResults
+        Successfull = 0
+        NotSuccessfull = 1
+    End Enum
+
+    Public Sub New(ByVal dir1 As String, ByVal dir2 As String)
+        Me.Dir1 = New DirectoryInfo(dir1)
+        Me.Dir2 = New DirectoryInfo(dir2)
+        'Both directories must exist... if not an exception is thrown
+        If (Not Me.Dir1.Exists) OrElse (Not Me.Dir2.Exists) Then Throw New DirectoryNotFoundException
+        Me.BeginSynchronization()
+    End Sub
+
+    Public Function BeginSynchronization() As SyncResults
+        Rtb("Start sync!", 3, True)
+        If Me.SyncProcess(Dir1, Dir2) = SyncResults.NotSuccessfull Then Return SyncResults.NotSuccessfull
+        Rtb("Sync1 complete! Change direction -><-", 3, True)
+        If Me.SyncProcess(Dir2, Dir1) = SyncResults.NotSuccessfull Then Return SyncResults.NotSuccessfull
+        Rtb("Sync2 complete! all done", 3, True)
+        Return SyncResults.Successfull
+    End Function
+
+
+    Private Function SyncProcess(
+        ByVal sourceDir As DirectoryInfo,
+        ByVal destinationDir As DirectoryInfo) As SyncResults
+        Try
+            Me.SyncDir(sourceDir, destinationDir)
+        Catch ex As Exception
+            Return SyncResults.NotSuccessfull
+        End Try
+        Return SyncResults.Successfull
+    End Function
+
+    Private Sub SyncDir(
+        ByVal sourceDir As DirectoryInfo,
+        ByVal destinationDir As DirectoryInfo)
+        Dim files As FileInfo() = sourceDir.GetFiles
+        Dim directories As DirectoryInfo() = sourceDir.GetDirectories
+        'Sync files...
+        For f As Integer = 0 To files.Length - 1
+            If File.Exists(destinationDir.FullName & "\" & files(f).Name) Then
+                Rtb("File: ", 0, False)
+                Rtb(files(f).Name, 2, False)
+                Rtb(" exists!", 1, False)
+                If files(f).LastWriteTime > File.GetLastAccessTime(destinationDir.FullName & "\" & files(f).Name) Then
+                    File.Copy(files(f).FullName, destinationDir.FullName & "\" & files(f).Name, True)
+                    Rtb(" but is Newer ---> copy!", 3, True)
+                    Rtb(files(f).LastWriteTime & " -- " & File.GetLastAccessTime(destinationDir.FullName & "\" & files(f).Name), 0, True)
+                Else
+                    Rtb(" and is up to date!", 1, True)
+                End If
+            Else
+                File.Copy(files(f).FullName, destinationDir.FullName & "\" & files(f).Name)
+                Rtb("File: ", 0, False)
+                Rtb(files(f).Name, 2, False)
+                Rtb(" not exist ---> copy!", 3, True)
+            End If
+        Next
+        'Sync directories
+        For d As Integer = 0 To directories.Length - 1
+            If Not Directory.Exists(destinationDir.FullName & "\" & directories(d).Name) Then
+                Rtb("Directory: ", 0, False)
+                Rtb(directories(d).Name, 2, False)
+                Rtb(" not exist ---> create!", 3, True)
+                Directory.CreateDirectory(destinationDir.FullName & "\" & directories(d).Name)
+            End If
+
+            Me.SyncDir(directories(d), New DirectoryInfo(destinationDir.FullName & "\" & directories(d).Name))
+        Next
+    End Sub
+
+    Private Sub Rtb(ByVal str As String, ByVal colour As Integer, ByVal newLine As Boolean)
+        With Form1.RichTextBox1
+            If colour = 1 Then
+                .SelectionColor = Color.YellowGreen
+            ElseIf colour = 2 Then
+                .SelectionColor = Color.LimeGreen
+            ElseIf colour = 3 Then
+                .SelectionColor = Color.Red
+            Else
+                .SelectionColor = Color.Black
+            End If
+            .SelectionFont = New Font(Form1.RichTextBox1.Font, FontStyle.Bold)
+            If newLine Then
+                .AppendText(str & vbNewLine)
+            Else
+                .AppendText(str)
+            End If
+        End With
+    End Sub
+
+End Class
